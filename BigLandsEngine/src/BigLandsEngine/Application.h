f5f4061c0da3fc47a700dc464ca141b255737562
BigLands <biglands65@gmail.com>
2023-03-20 13:47:12 -0400

    Development update 8 Window Events

--- BigLandsEngine/src/BigLandsEngine/Application.h
+++ BigLandsEngine/src/BigLandsEngine/Application.h
@@ -2,6 +2,7 @@
 
 #include "Core.h"
 #include "Events/Event.h"
+#include "Events/ApplicationEvent.h"
 #include "Window.h"
 
 namespace BigLandsEngine {
@@ -13,7 +14,9 @@ namespace BigLandsEngine {
 		virtual ~Application();
 
 		void Run();
+		void OnEvent(Event& e);
 	private:
+		bool OnWindowClose(WindowCloseEvent& e);
 		std::unique_ptr<Window> m_Window;
 		bool m_Running = true;
 	};
